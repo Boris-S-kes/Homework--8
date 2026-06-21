22da9d126f0fdbce4b315d34b9a0de0035a7efae
msi <https://github.com/Boris-S-kes/Homework--8.git>
2026-06-22 00:04:00 +0400

    dz 8 revision 2

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.lang.reflect.Array;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,60 +9,81 @@ public class Main {
         int[] numbers = new int[5];
 
         System.out.println(" track2");
+
         for (int i = 0; i < star.length; i++) {
-            System.out.print(star[i] + ",");
-
-        }
-        for (int b = 0; b < work.length; b++) {
-            System.out.print(work[b] + ",");
-
-        }
-        for (int c = 0; c < numbers.length; c++) {
-            System.out.print(numbers[c] + ",");
+            if (i == star.length - 1) {
+                System.out.print(star[i]);
+            } else {
+                System.out.print(star[i] + ",");
+            }
         }
         System.out.println();
 
-        System.out.println(" track 3");
+
+        for (int s = 0; s < work.length; s++) {
+            if (s == work.length - 1) {
+                System.out.print(work[s]);
+            } else {
+                System.out.print(work[s] + ",");
+            }
+        }
+        System.out.println();
+
+        for (int e = 0; e < numbers.length; e++) {
+            if (e == numbers.length - 1) {
+                System.out.print(numbers[e]);
+            }
+        }
+            System.out.println();
+
+
+            System.out.println(" track 3");
+
+
+
         for (int w = star.length - 1; w >= 0; w--) {
-            System.out.print(star[w]);
-            if (w >-1) {
-                System.out.print(" , ");
+            if (w == 0) {
+                System.out.print(star[w]);
+            } else {
+                System.out.print(star[w] + " , ");
             }
-
-        }
-        for (int s = work.length - 1; s >= 0; s--) {
-            System.out.print(work[s]);
-            if (s >-1) {
-                System.out.print(" , ");
-            }
-        }
-        for (int z = numbers.length - 1; z >= 0; z--) {
-            System.out.print(numbers[z]);
-
-            if (z > -1) {
-                System.out.print(",");
-            }
-
         }
         System.out.println();
+
+        for (int s = work.length - 1; s >= 0; s--) {
+            if (s == 0) {
+                System.out.print(work[s]);
+            } else {
+                System.out.print(work[s] + " , ");
+            }
+        }
+        System.out.println();
+
+        for (int z = numbers.length - 1; z >= 0; z--) {
+            if (z == 0) {
+                System.out.print(numbers[z]);
+            } else {
+                System.out.print(numbers[z] + ",");
+            }
+        }
+
+        System.out.println();
+
 
         System.out.println(" track 4");
-        int[] star2 = new int[]{1, 2, 3};
-        for (int r = 0; r < star2.length; r++) {
-            if (star2[r] % 2 != 0) {
-                star2[r] += 1;
-
-                System.out.println(star2[r]);
 
 
+
+        int[] arr = new int[]{1, 2, 3};
+            for (int r = 0; r < arr.length; r++) {
+                arr[r] = r + 1;
+                if (arr[r] % 2 != 0) {
+                    arr[r] += 1;
+                }
             }
-        }
-    }
-}
 
+                    System.out.println(Arrays.toString(arr));
 
-
-
-
-
+                }
+            }
 
