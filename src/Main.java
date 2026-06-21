41cc79ec48bab8eeac416f8b6899c97ea077663d
msi <https://github.com/Boris-S-kes/Homework--8.git>
2026-06-21 17:05:55 +0400

    dz 8 revision

--- src/Main.java
+++ src/Main.java
@@ -9,54 +9,56 @@ public class Main {
 
         System.out.println(" track2");
         for (int i = 0; i < star.length; i++) {
-            System.out.println(star[i]);
+            System.out.print(star[i] + ",");
+
         }
         for (int b = 0; b < work.length; b++) {
-            System.out.println(work[b]);
+            System.out.print(work[b] + ",");
+
         }
         for (int c = 0; c < numbers.length; c++) {
-            System.out.println(numbers[c]);
+            System.out.print(numbers[c] + ",");
         }
+        System.out.println();
+
         System.out.println(" track 3");
         for (int w = star.length - 1; w >= 0; w--) {
-            if (w < star.length - 1) {
-                System.out.println(" , ");
+            System.out.print(star[w]);
+            if (w >-1) {
+                System.out.print(" , ");
             }
-            System.out.println(star[w]);
+
         }
         for (int s = work.length - 1; s >= 0; s--) {
-            if (s < work.length - 1) {
-                System.out.println(" , ");
+            System.out.print(work[s]);
+            if (s >-1) {
+                System.out.print(" , ");
             }
-            System.out.println(work[s]);
         }
         for (int z = numbers.length - 1; z >= 0; z--) {
-            if (z < numbers.length - 1) {
-                System.out.println(",");
+            System.out.print(numbers[z]);
+
+            if (z > -1) {
+                System.out.print(",");
             }
-            System.out.println(numbers[z]);
+
         }
+        System.out.println();
+
         System.out.println(" track 4");
-        for (int x = 0; x < star.length - 1; x++) {
-            if (star[x] % 2 != 0) {
-                star[x] += 1;
-                System.out.println(star[x] + " , ");
-            }
-            for (int c = 0; c < work.length - 1; c++) {
-                if (work[c] % 2 != 0) {
-                    work[c] += 1;
-                    System.out.println(work[c] + " ,");
-                }
-                for (int v = 0; v < numbers.length - 1; v++) {
-                    if (numbers[v] % 2 != 0) {
-                        numbers[v] += 1;
-                        System.out.println(numbers[v] + ",");
-                    }
-                }
+        int[] star2 = new int[]{1, 2, 3};
+        for (int r = 0; r < star2.length; r++) {
+            if (star2[r] % 2 != 0) {
+                star2[r] += 1;
+
+                System.out.println(star2[r]);
+
+
             }
         }
     }
 }
+
 
 
 
